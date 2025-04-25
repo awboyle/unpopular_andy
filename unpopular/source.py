1d211cbac9b1c741e3f45c50bd7720f188aa28e5
awboyle <70535690+awboyle@users.noreply.github.com>
2025-04-25 14:09:45 -0400

    Update source.py

--- unpopular/source.py
+++ unpopular/source.py
@@ -16,7 +16,7 @@ class Source(object):
 
     """
 
-    def __init__(self, hdu, path, remove_bad=True, verbose=True, 
+    def __init__(self, hdu, remove_bad=True, verbose=True, 
                  provenance='TessCut', quality=None, bkg_subtract=False, bkg_n=100,
                  time_path=None, flux_path=None, ferr_path=None):
         
